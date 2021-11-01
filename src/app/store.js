1641fb70a54eb9c393b7e40a7d1fefdc5e7e5aac
SaahilMahato <saahilmahato72@gmail.com>
2021-11-01 15:40:35 +0545

    removed redux

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReducer from '../features/car/CarSlice';
-
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-    },
-});
